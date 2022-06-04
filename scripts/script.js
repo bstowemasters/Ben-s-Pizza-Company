7591018655b543e6857d0d2d3a384e79f8d726ff
bstowemasters <benjamin.stowe-masters20@bathspa.ac.uk>
2022-06-04 17:51:24 +0100

    Removed sticky nav from mobile views

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,19 @@
 function scrollPage(value) {
     const element = document.getElementById(value);
-    element.scrollIntoView();       /* Scroll to selected tab */
-    window.scrollBy(0, -550)
+    console.log(value);
+
+    window.scrollTo({   /* Smooth scrolling function with offset */
+        behavior: 'smooth', /* Adds smooth behaviour */
+        
+        top:                /* Calculates top amount by working out element location and adding offset to position */
+        document.querySelector("." + value).getBoundingClientRect().top -
+        document.body.getBoundingClientRect().top -
+        + 450              /* Adds on spacing above container for more accurate positioning. */
+    });
+    
+
+    // Original method which doesn't allow smooth scrolling.
+    // element.scrollIntoView();       /* Scroll to selected tab */
+    // document.scrollBy(0, -550);
+
 }
